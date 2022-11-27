2d39c94498a3bca17e7bdeff789974b9b9ef4d8d
MaryanaAm <marydeaddoll@gmail.com>
2022-11-27 21:12:11 +0300

    Add chainges on 003 file

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 31;
-int numB = 10;
+﻿int numA = new Random().Next(1, 20); // Программа выберет случайное число от 1 до 19
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 20);
+Console.WriteLine(numB);
 int sum = numA + numB;
 Console.WriteLine(sum);
