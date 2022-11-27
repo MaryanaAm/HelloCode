fa9b4ebee330aabe80c437a8cde0343901373382
MaryanaAm <marydeaddoll@gmail.com>
2022-11-27 21:41:09 +0300

    Add dif in file 002

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя ");
+string usermame = Console.ReadLine();
 Console.WriteLine("Привет, ");
-Console.WriteLine(username);
-
+Console.WriteLine(usermame);
