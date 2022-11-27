e69ab2b7446b667c2ef3485d8f18e10cb63821d7
MaryanaAm <marydeaddoll@gmail.com>
2022-11-27 21:58:07 +0300

    Add 005 file

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string usermame = Console.ReadLine();
+
+if(usermame.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(usermame);
+}
